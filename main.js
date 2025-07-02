e586ad3e2cb0c85c4d2e39cb9baaf643bfd4c28f
drifansah <127324766+drifansah@users.noreply.github.com>
2025-07-03 01:39:11 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const endpoint = "https://backendwebiot-production.up.railway.app";
+
+function setTaman(taman, status) {
+  fetch(`${endpoint}/api/${taman}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ status })
+  }).then(() => getTamanStatus(taman));
+}
+
+function getTamanStatus(taman) {
+  fetch(`${endpoint}/api/${taman}`)
+    .then(res => res.json())
+    .then(data => {
+      const img = document.getElementById(`waterTamanImage${taman.replace('taman', '')}`);
+      if (data.status === "ON") {
+        img.src = "assets/watering-on.png";
+      } else {
+        img.src = "assets/watering-off.png";
+      }
+    });
+}
+
+['taman1', 'taman2', 'taman3', 'taman4'].forEach(getTamanStatus);
